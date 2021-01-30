4d162ae507161b10ad99b1aaf894858278e6a2a3
Thomas Nijssen <thomasnijssen97@gmail.com>
2021-01-29 21:16:13 -0800

    usability improvements

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ include 'functions.php';
 <legend>Relative to container</legend>
 <label>Title: <input type="text" name="titlesel" value="<?= htmlspecialchars($_GET['titlesel'] ?? '') ?>" class="selector"></label><br>
 <label>Date: <input type="text" name="datesel" value="<?= htmlspecialchars($_GET['datesel'] ?? '') ?>" class="selector"></label>&nbsp;&nbsp;
-<label>Date format: <input type="text" name="datefmt" value="<?= htmlspecialchars($_GET['datefmt'] ?? '') ?>"></label><br>
+<label><a href="https://www.php.net/manual/en/datetime.createfromformat.php" target="_blank" title="PHP date format">Date format</a>: <input type="text" name="datefmt" value="<?= htmlspecialchars($_GET['datefmt'] ?? '') ?>"></label><br>
 <label>Content: <input type="text" name="contentsel" value="<?= htmlspecialchars($_GET['contentsel'] ?? '') ?>" class="selector"></label> (blank for everything else)<br>
 <div id="removesels">
 <?php if (isset($_GET['removesel']) && is_array($_GET['removesel'])) { foreach ($_GET['removesel'] as $rs) { ?>
@@ -28,13 +28,23 @@ include 'functions.php';
 </fieldset>
 <br>
 <input type="submit" value="Apply"><br>
-<textarea rows="2" cols="80" readonly><?= htmlspecialchars(absurl("feed.php") . '?' . $_SERVER['QUERY_STRING']) ?></textarea>
 <!-- <input type="button" value="Preview" id="previewbtn"> -->
 </form>
-<iframe id="previewframe" style="width: 100%; height: 50%;" src="feed.php?preview=1&amp;<?= htmlspecialchars($_SERVER['QUERY_STRING'] ?? '') ?>"></iframe>
-<br>
-<iframe id="pageframe" style="width: 100%; height: 50%;" src="proxy.php?<?= htmlspecialchars($_SERVER['QUERY_STRING'] ?? '') ?>"></iframe>
 
+<?php if (isset($_GET['containersel']) && isset($_GET['url']) && !empty($_GET['containersel'])) { ?>
+<label>RSS link: <textarea rows="2" cols="80" readonly><?= htmlspecialchars(absurl("feed.php") . '?' . $_SERVER['QUERY_STRING']) ?></textarea></label>
+
+<h2>RSS feed preview</h2>
+<iframe id="previewframe" style="width: 100%; height: 50%;" src="feed.php?preview=1&amp;<?= htmlspecialchars($_SERVER['QUERY_STRING'] ?? '') ?>"></iframe>
+<?php } ?>
+
+<br>
+
+<?php if (isset($_GET['url'])) { ?>
+<h2>Page preview</h2>
+<iframe id="pageframe" style="width: 100%; height: 50%;" src="proxy.php?<?= htmlspecialchars($_SERVER['QUERY_STRING'] ?? '') ?>"></iframe>
+<p>(hint: use inspect element and look at the <tt>data-selector</tt> attributes)</p>
+<?php } ?>
 
 <script type="text/javascript">
 $("#addremovesel").click(function () {
